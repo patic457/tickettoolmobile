a9d5414fc2e4f8bf5e6e0831ec19ee410eb45224
patic457 <patic457@gmail.com>
2022-10-11 17:21:51 +0700

    Create TicketDetailWidget.dart

---
+++ lib/widgets/TicketDetailWidget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class TicketDetailWidget extends StatefulWidget {
+  const TicketDetailWidget({super.key});
+
+  @override
+  State<TicketDetailWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<TicketDetailWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
